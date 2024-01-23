cc06d79051bd07957717efdb23e691bae3ed5c9d
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2024-01-23 04:41:45 +0100

    Update RadioCrudController.php

--- src/Controller/Admin/Media/RadioCrudController.php
+++ src/Controller/Admin/Media/RadioCrudController.php
@@ -3,10 +3,17 @@
 namespace App\Controller\Admin\Media;
 
 use App\Entity\Radio;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\UrlField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use Adeliom\EasyMediaBundle\Admin\Field\EasyMediaField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class RadioCrudController extends AbstractCrudController
 {
@@ -15,14 +22,36 @@ class RadioCrudController extends AbstractCrudController
         return Radio::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        yield FormField::addFieldset('General Information');
+        yield TextField::new('Title')
+        ->setColumns(7);
+        yield DateField::new('Date')
+        ->setColumns(1);
+
+        yield FormField::addRow();
+        yield EasyMediaField::new('Image')
+        ->setFormTypeOption("restrictions_uploadTypes", ["image/*"])
+        ->setFormTypeOption("restrictions_path", "Radio/Thumbnails/")
+        ->setColumns(1);
+
+        yield FormField::addFieldset('Artists');
+        yield AssociationField::new('Artists')
+        ->autocomplete()
+        ->hideOnIndex()
+        ->setColumns(8);
+        
+        yield FormField::addFieldset('Resources');
+        yield UrlField::new('YouTubeUrl', 'YouTube')
+        ->hideOnIndex()
+        ->setColumns(8);
     }
-    */
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setSearchFields(['Title', 'Date', 'Artists.Name'])
+            ->setDefaultSort(['Date' => 'DESC']);
+    }
 }

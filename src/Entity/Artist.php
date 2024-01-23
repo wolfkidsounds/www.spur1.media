b37c368b33b2069d88292cad27e5ae1a66d64bd1
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2024-01-23 04:41:50 +0100

    Update Artist.php

--- src/Entity/Artist.php
+++ src/Entity/Artist.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\ArtistRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ArtistRepository::class)]
@@ -15,6 +17,19 @@ class Artist
 
     #[ORM\Column(length: 255)]
     private ?string $Name = null;
+
+    #[ORM\ManyToMany(targetEntity: Radio::class, mappedBy: 'Artists')]
+    private Collection $Radios;
+
+    public function __construct()
+    {
+        $this->Radios = new ArrayCollection();
+    }
+
+    public function __toString()
+    {
+        return $this->getName();
+    }
 
     public function getId(): ?int
     {
@@ -29,6 +44,33 @@ class Artist
     public function setName(string $Name): static
     {
         $this->Name = $Name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Radio>
+     */
+    public function getRadios(): Collection
+    {
+        return $this->Radios;
+    }
+
+    public function addRadio(Radio $radio): static
+    {
+        if (!$this->Radios->contains($radio)) {
+            $this->Radios->add($radio);
+            $radio->addArtist($this);
+        }
+
+        return $this;
+    }
+
+    public function removeRadio(Radio $radio): static
+    {
+        if ($this->Radios->removeElement($radio)) {
+            $radio->removeArtist($this);
+        }
 
         return $this;
     }

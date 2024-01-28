11871290e0edd2bd8c41a48417373d053b003459
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2024-01-28 22:43:43 +0100

    Create Windowlicker.php

---
+++ src/Entity/Windowlicker.php
@@ -0,0 +1,188 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\WindowlickerRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: WindowlickerRepository::class)]
+class Windowlicker
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Title = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $Image = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $Date = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $YouTubeUrl = null;
+
+    #[ORM\ManyToMany(targetEntity: Artist::class, inversedBy: 'Windowlickers')]
+    private Collection $Artists;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Slug = null;
+
+    #[ORM\ManyToMany(targetEntity: Tag::class, inversedBy: 'Windowlickers')]
+    private Collection $Tags;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $Description = null;
+
+    #[ORM\ManyToOne(inversedBy: 'Windowlickers')]
+    private ?Location $Location = null;
+
+    public function __construct()
+    {
+        $this->Artists = new ArrayCollection();
+        $this->Tags = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->Title;
+    }
+
+    public function setTitle(string $Title): static
+    {
+        $this->Title = $Title;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->Image;
+    }
+
+    public function setImage(?string $Image): static
+    {
+        $this->Image = $Image;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->Date;
+    }
+
+    public function setDate(\DateTimeInterface $Date): static
+    {
+        $this->Date = $Date;
+
+        return $this;
+    }
+
+    public function getYouTubeUrl(): ?string
+    {
+        return $this->YouTubeUrl;
+    }
+
+    public function setYouTubeUrl(?string $YouTubeUrl): static
+    {
+        $this->YouTubeUrl = $YouTubeUrl;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Artist>
+     */
+    public function getArtists(): Collection
+    {
+        return $this->Artists;
+    }
+
+    public function addArtist(Artist $artist): static
+    {
+        if (!$this->Artists->contains($artist)) {
+            $this->Artists->add($artist);
+        }
+
+        return $this;
+    }
+
+    public function removeArtist(Artist $artist): static
+    {
+        $this->Artists->removeElement($artist);
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->Slug;
+    }
+
+    public function setSlug(string $Slug): static
+    {
+        $this->Slug = $Slug;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Tag>
+     */
+    public function getTags(): Collection
+    {
+        return $this->Tags;
+    }
+
+    public function addTag(Tag $tag): static
+    {
+        if (!$this->Tags->contains($tag)) {
+            $this->Tags->add($tag);
+        }
+
+        return $this;
+    }
+
+    public function removeTag(Tag $tag): static
+    {
+        $this->Tags->removeElement($tag);
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->Description;
+    }
+
+    public function setDescription(?string $Description): static
+    {
+        $this->Description = $Description;
+
+        return $this;
+    }
+
+    public function getLocation(): ?Location
+    {
+        return $this->Location;
+    }
+
+    public function setLocation(?Location $Location): static
+    {
+        $this->Location = $Location;
+
+        return $this;
+    }
+}

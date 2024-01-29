390b0a4b6f512c604ba3dcae5570d2d6feb1b1c8
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2024-01-29 23:13:58 +0100

    Create Version20240129014750.php

---
+++ migrations/Version20240129014750.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240129014750 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE artist ADD you_tube_url VARCHAR(255) DEFAULT NULL, ADD soundcloud_url VARCHAR(255) DEFAULT NULL, ADD facebook_url VARCHAR(255) DEFAULT NULL, ADD instagram_url VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE artist DROP you_tube_url, DROP soundcloud_url, DROP facebook_url, DROP instagram_url');
+    }
+}

a2b31a99aaafe124b51215e90f5c7fa3bf191dc5
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2024-02-01 15:48:15 +0100

    Blurry Image Loading

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,2 @@
 import './simplebar.js';
+import './blurry-load.js';
---
+++ assets/js/blurry-load.js
@@ -0,0 +1,6 @@
+import './blurry-load/blurry-load.css';
+import BlurryImageLoad from './blurry-load/blurry-load.js';
+
+const blurryImageLoad = new BlurryImageLoad();
+
+blurryImageLoad.load();

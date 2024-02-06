a72401c08854a2412ea4a503fdb9608d4ea89992
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2024-02-06 06:39:35 +0100

    Update Teletime.php

--- src/Entity/Teletime.php
+++ src/Entity/Teletime.php
@@ -2,69 +2,19 @@
 
 namespace App\Entity;
 
-use App\Entity\Artist;
-use Doctrine\DBAL\Types\Types;
+use App\Entity\Post;
 use Doctrine\ORM\Mapping as ORM;
-use ApiPlatform\Metadata\ApiResource;
 use App\Repository\TeletimeRepository;
-use Doctrine\Common\Collections\Collection;
-use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping\InheritanceType;
+use Doctrine\ORM\Mapping\DiscriminatorMap;
+use Doctrine\ORM\Mapping\DiscriminatorColumn;
 
 #[ORM\Entity(repositoryClass: TeletimeRepository::class)]
-#[ApiResource()]
-class Teletime
+#[ORM\Table(name: 'post')]
+class Teletime extends Post
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
-
-    #[ORM\Column(length: 255, nullable: true)]
-    private ?string $Title = null;
-
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $YouTubeUrl = null;
-
-    #[ORM\Column(length: 255, nullable: true)]
-    private ?string $Image = null;
-
-    #[ORM\Column(type: Types::DATE_MUTABLE)]
-    private ?\DateTimeInterface $Date = null;
-
-    #[ORM\ManyToMany(targetEntity: Artist::class, inversedBy: 'Teletimes')]
-    private Collection $Artists;
-
-    #[ORM\Column(length: 255)]
-    private ?string $Slug = null;
-
-    #[ORM\Column(type: Types::TEXT, nullable: true)]
-    private ?string $Description = null;
-
-    #[ORM\ManyToMany(targetEntity: Tag::class, inversedBy: 'Teletimes')]
-    private Collection $Tags;
-
-    public function __construct()
-    {
-        $this->Artists = new ArrayCollection();
-        $this->Tags = new ArrayCollection();
-    }
-
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
-
-    public function getTitle(): ?string
-    {
-        return $this->Title;
-    }
-
-    public function setTitle(?string $Title): static
-    {
-        $this->Title = $Title;
-
-        return $this;
-    }
 
     public function getYouTubeUrl(): ?string
     {
@@ -74,102 +24,6 @@ class Teletime
     public function setYouTubeUrl(?string $YouTubeUrl): static
     {
         $this->YouTubeUrl = $YouTubeUrl;
-
-        return $this;
-    }
-
-    public function getImage(): ?string
-    {
-        return $this->Image;
-    }
-
-    public function setImage(string $Image): static
-    {
-        $this->Image = $Image;
-
-        return $this;
-    }
-
-    public function getDate(): ?\DateTimeInterface
-    {
-        return $this->Date;
-    }
-
-    public function setDate(\DateTimeInterface $Date): static
-    {
-        $this->Date = $Date;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Artist>
-     */
-    public function getArtists(): Collection
-    {
-        return $this->Artists;
-    }
-
-    public function addArtist(Artist $artist): static
-    {
-        if (!$this->Artists->contains($artist)) {
-            $this->Artists->add($artist);
-        }
-
-        return $this;
-    }
-
-    public function removeArtist(Artist $artist): static
-    {
-        $this->Artists->removeElement($artist);
-
-        return $this;
-    }
-
-    public function getSlug(): ?string
-    {
-        return $this->Slug;
-    }
-
-    public function setSlug(string $Slug): static
-    {
-        $this->Slug = $Slug;
-
-        return $this;
-    }
-
-    public function getDescription(): ?string
-    {
-        return $this->Description;
-    }
-
-    public function setDescription(?string $Description): static
-    {
-        $this->Description = $Description;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Tag>
-     */
-    public function getTags(): Collection
-    {
-        return $this->Tags;
-    }
-
-    public function addTag(Tag $tag): static
-    {
-        if (!$this->Tags->contains($tag)) {
-            $this->Tags->add($tag);
-        }
-
-        return $this;
-    }
-
-    public function removeTag(Tag $tag): static
-    {
-        $this->Tags->removeElement($tag);
 
         return $this;
     }

ff7a31819e9d341fdb77ec27e08262cb6c49f77b
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2024-02-06 06:39:50 +0100

    Create PostRepository.php

---
+++ src/Repository/PostRepository.php
@@ -0,0 +1,24 @@
+<?php //src/Repository/PostRepository.php
+
+namespace App\Repository;
+
+use App\Entity\Post;
+use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+
+
+/**
+ * @extends ServiceEntityRepository<Post>
+ *
+ * @method Post|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Post|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Post[]    findAll()
+ * @method Post[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class PostRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Post::class);
+    }
+}

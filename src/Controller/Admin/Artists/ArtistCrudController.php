373519fa03a508d2ca62efdf2269b5b465db32d2
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2024-02-11 11:38:30 +0100

    Update ArtistCrudController.php

--- src/Controller/Admin/Artists/ArtistCrudController.php
+++ src/Controller/Admin/Artists/ArtistCrudController.php
@@ -2,13 +2,15 @@
 
 namespace App\Controller\Admin\Artists;
 
+use App\Entity\Link;
 use App\Entity\Artist;
-use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\UrlField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use Adeliom\EasyMediaBundle\Admin\Field\EasyMediaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
@@ -22,14 +24,15 @@ class ArtistCrudController extends AbstractCrudController
 
     public function configureFields(string $pageName): iterable
     {
+        yield FormField::addFieldset('General Information');
         yield TextField::new('Name');
 
         yield SlugField::new('Slug')
             ->setTargetFieldName(['Name'])
             ->setColumns(8)
             ->hideOnIndex();
-            
-        yield FormField::addRow();
+
+        yield FormField::addFieldset('Details');
         yield EasyMediaField::new('Image')
             ->setFormTypeOption('restrictions_uploadTypes', ["image/*"])
             ->setFormTypeOption('restrictions_path', 'Artists/Thumbnails/')
@@ -42,33 +45,16 @@ class ArtistCrudController extends AbstractCrudController
             ->hideOnIndex()
             ->setColumns(8);
 
-        yield FormField::addRow();
-        yield AssociationField::new('Owner')
-            ->hideOnIndex()
-            ->setColumns(8);
+        yield FormField::addFieldset('Links');
+        yield CollectionField::new('Links')
+            ->allowAdd()
+            ->allowDelete()
+            ->setEntryIsComplex()
+            ->useEntryCrudForm()
+            ->hideOnIndex();
 
-        yield FormField::addRow();
-        yield UrlField::new('SpotifyUrl')
-            ->hideOnIndex()
-            ->setColumns(2);
-        yield UrlField::new('SoundcloudUrl')
-            ->hideOnIndex()
-            ->setColumns(2);
-        yield UrlField::new('MixcloudUrl')
-            ->hideOnIndex()
-            ->setColumns(2);
-        yield UrlField::new('BandcampUrl')
-            ->hideOnIndex()
-            ->setColumns(2);
-        yield FormField::addRow();
-        yield UrlField::new('YouTubeUrl')
-            ->hideOnIndex()
-            ->setColumns(2);
-        yield UrlField::new('FacebookUrl')
-            ->hideOnIndex()
-            ->setColumns(2);
-        yield UrlField::new('InstagramUrl')
-            ->hideOnIndex()
-            ->setColumns(2);
+        yield FormField::addFieldset('Settings');
+        yield AssociationField::new('Owner')
+            ->setColumns(8);
     }
 }

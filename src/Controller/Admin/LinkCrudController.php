1312e922cfb1afcfbadb4b5b9f0bf5e879cae4e3
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2024-02-11 11:38:38 +0100

    Create LinkCrudController.php

---
+++ src/Controller/Admin/LinkCrudController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Link;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\UrlField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+
+class LinkCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Link::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        yield AssociationField::new('Type')
+        ->setColumns(8);
+        yield FormField::addRow();
+        yield UrlField::new('Url')
+        ->setColumns(8);
+    }
+}

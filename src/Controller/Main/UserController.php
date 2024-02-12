57d8bee30e2bab3196c469c2786f0c6397b35dc2
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2024-02-12 13:58:20 +0100

    Update UserController.php

--- src/Controller/Main/UserController.php
+++ src/Controller/Main/UserController.php
@@ -2,31 +2,46 @@
 
 namespace App\Controller\Main;
 
+use App\Entity\User;
 use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends MainController
 {
-    #[Route('/user', name: 'app_main_user_index', methods: ['GET'])]
+    #[Route('/members', name: 'app_main_user_index', methods: ['GET'])]
     public function user_index(UserRepository $repository): Response
     {
         $posts = $repository->findBy([], ['Date' => 'DESC']);
 
         return $this->render('section/main/page/user/index.html.twig', [
             'posts' => $posts,
-            'title' => 'User',
+            'title' => 'Users',
         ]);
     }
 
-    #[Route('/user/{slug}', name: 'app_main_user_single', methods: ['GET'])]
+    #[Route('/member/{slug}', name: 'app_main_user_single', methods: ['GET'])]
     public function user_single(string $slug, UserRepository $repository): Response
     {
         $post = $repository->findOneBy(['Slug' => $slug]);
 
         return $this->render('section/main/page/user/single.html.twig', [
             'post' => $post,
-            'title' => 'User',
+            'title' => $post->getName(),
         ]);
-    } 
+    }
+
+    #[Route('/user/artists', name: 'app_main_user_artists', methods: ['GET'])]
+    public function user_artists(): Response
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+        $artists = $user->getArtists();
+
+        return $this->render('section/main/page/user/my-artists.html.twig', [
+            'user' => $user,
+            'artists' => $artists,
+            'title' => 'My Artists',
+        ]);
+    }
 }

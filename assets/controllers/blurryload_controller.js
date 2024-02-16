f0b2722a0e0addaf8ac1d8a285225b654de1e272
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2024-02-16 12:42:30 +0100

    Update blurryload_controller.js

--- assets/controllers/blurryload_controller.js
+++ assets/controllers/blurryload_controller.js
@@ -3,16 +3,6 @@ import '../js/app/blurry-load/blurry-load.css';
 import BlurryImageLoad from '../js/app/blurry-load/blurry-load.js';
 
 const blurryImageLoad = new BlurryImageLoad();
-
-/*
- * This is an example Stimulus controller!
- *
- * Any element with a data-controller="hello" attribute will cause
- * this controller to be executed. The name "hello" comes from the filename:
- * hello_controller.js -> "hello"
- *
- * Delete this file or adapt it for your use!
- */
 export default class extends Controller {
     initialize() {
         blurryImageLoad.load();

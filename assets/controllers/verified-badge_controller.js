73a89e24414e062f18e34e029e991df66f27c7fa
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2024-02-17 19:42:37 +0100

    Update verified-badge_controller.js

--- assets/controllers/verified-badge_controller.js
+++ assets/controllers/verified-badge_controller.js
@@ -1,8 +1,8 @@
 import { Controller } from '@hotwired/stimulus';
+import * as bootstrap from 'bootstrap';
 
 export default class extends Controller {
     connect() {
-        console.log('Connected!');
         const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
         const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
     }

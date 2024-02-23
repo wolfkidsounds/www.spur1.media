29a79930995c36e926a69e5ece86bac5ff4db2d6
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2024-02-23 19:37:27 +0100

    Update Homepage

--- src/Controller/Main/MainController.php
+++ src/Controller/Main/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Main;
 
 use App\Controller\BaseController;
+use App\Repository\ArtistRepository;
 use App\Repository\PostRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,9 +12,13 @@ use Symfony\Component\Routing\Attribute\Route;
 class MainController extends BaseController
 {
     #[Route('/', name: 'app_main_index')]
-    public function index(): Response
+    public function index(ArtistRepository $artistRepository, PostRepository $postRepository): Response
     {
+        $artists = $artistRepository->findBy([], ['createdAt' => 'DESC'], 6);
+        $posts = $postRepository->findBy([], ['createdAt' => 'DESC'], 6);
         return $this->render('section/main/page/index.html.twig', [
+            'artists' => $artists,
+            'posts' => $posts,
             'title' => 'Home',
         ]);
     }

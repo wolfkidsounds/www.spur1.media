739052964ea2b1806328bacf19459fa9bd1961d0
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2024-03-02 22:08:51 +0100

    Update Builder.php

--- src/Menu/Builder.php
+++ src/Menu/Builder.php
@@ -2,7 +2,6 @@
 
 namespace App\Menu;
 
-use App\Entity\User;
 use Knp\Menu\ItemInterface;
 use Knp\Menu\FactoryInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -25,14 +24,19 @@ class Builder extends AbstractController
             'extras' => [
                 'icon' => 'fa-solid fa-home w-16',
                 'class' => '',
-            ]
+            ],
+            'linkAttributes' => [
+                'data-bs-toggle' => 'tooltip',
+                'data-bs-title' => 'Home',
+                'data-bs-placement' => 'right',
+            ],
         ]);
 
         $menu->addChild('divider_custom_1', [
             'divider' => true, 
             'extras' => [
                 'divider' => true
-            ]
+            ],
         ]);
 
         $menu->addChild('Orbiter Session', [
@@ -40,7 +44,12 @@ class Builder extends AbstractController
             'extras' => [
                 'icon' => 'fa-solid fa-record-vinyl w-16',
                 'class' => '',
-            ]
+            ],
+            'linkAttributes' => [
+                'data-bs-toggle' => 'tooltip',
+                'data-bs-title' => 'Orbiter Session',
+                'data-bs-placement' => 'right',
+            ],
         ]);
 
         $menu->addChild('Podcast', [
@@ -48,7 +57,12 @@ class Builder extends AbstractController
             'extras' => [
                 'icon' => 'fa-solid fa-podcast w-16',
                 'class' => '',
-            ]
+            ],
+            'linkAttributes' => [
+                'data-bs-toggle' => 'tooltip',
+                'data-bs-title' => 'Podcast',
+                'data-bs-placement' => 'right',
+            ],
         ]);
 
         $menu->addChild('Radio', [
@@ -56,7 +70,12 @@ class Builder extends AbstractController
             'extras' => [
                 'icon' => 'fa-solid fa-radio w-16',
                 'class' => '',
-            ]
+            ],
+            'linkAttributes' => [
+                'data-bs-toggle' => 'tooltip',
+                'data-bs-title' => 'Radio',
+                'data-bs-placement' => 'right',
+            ],
         ]);
 
         $menu->addChild('Teletime', [
@@ -64,35 +83,62 @@ class Builder extends AbstractController
             'extras' => [
                 'icon' => 'fa-solid fa-phone w-16',
                 'class' => '',
-            ]
+            ],
+            'linkAttributes' => [
+                'data-bs-toggle' => 'tooltip',
+                'data-bs-title' => 'Teletime',
+                'data-bs-placement' => 'right',
+            ],
         ]);
 
         $menu->addChild('Windowlicker', [
             'route' => 'app_main_windowlicker_index',
             'extras' => [
+                'class' => '',
                 'icon' => 'fa-brands fa-windows w-16',
-            ]
+            ],
+            'linkAttributes' => [
+                'data-bs-toggle' => 'tooltip',
+                'data-bs-title' => 'Windowlicker',
+                'data-bs-placement' => 'right',
+            ],
         ]);
 
         $menu->addChild('Neben Der Spur', [
             'route' => 'app_main_neben-der-spur_index',
             'extras' => [
                 'icon' => 'fa-solid fa-shuffle w-16',
-            ]
+            ],
+            'linkAttributes' => [
+                'data-bs-toggle' => 'tooltip',
+                'data-bs-title' => 'Neben Der Spur',
+                'data-bs-placement' => 'right',
+            ],
         ]);
 
         $menu->addChild('Rec Session', [
             'route' => 'app_main_rec-session_index',
             'extras' => [
                 'icon' => 'fa-solid fa-circle-play w-16',
-            ]
+            ],
+            'linkAttributes' => [
+                'data-bs-toggle' => 'tooltip',
+                'data-bs-title' => 'Rec Session',
+                'data-bs-placement' => 'right',
+            ],
         ]);
 
         $menu->addChild('Interview', [
             'route' => 'app_main_interview_index',
             'extras' => [
+                'class' => 'pe-none opacity-25',
                 'icon' => 'fa-solid fa-microphone w-16',
-            ]
+            ],
+            'linkAttributes' => [
+                'data-bs-toggle' => 'tooltip',
+                'data-bs-title' => 'Interview',
+                'data-bs-placement' => 'right',
+            ],
         ]);
 
         $menu->addChild('divider_custom_2', [
@@ -107,6 +153,31 @@ class Builder extends AbstractController
             'extras' => [
                 'icon' => 'fa-solid fa-icons w-16',
                 'class' => '',
+            ],
+            'linkAttributes' => [
+                'data-bs-toggle' => 'tooltip',
+                'data-bs-title' => 'Artists',
+                'data-bs-placement' => 'right',
+            ],
+        ]);
+
+        $menu->addChild('Crews', [
+            'route' => 'app_crew_index',
+            'extras' => [
+                'icon' => 'fa-solid fa-users w-16',
+                'class' => 'opacity-25 pe-none',
+            ],
+            'linkAttributes' => [
+                'data-bs-toggle' => 'tooltip',
+                'data-bs-title' => 'Crews',
+                'data-bs-placement' => 'right',
+            ],
+        ]);
+
+        $menu->addChild('divider_custom_3', [
+            'divider' => true, 
+            'extras' => [
+                'divider' => true
             ]
         ]);
 
@@ -114,28 +185,24 @@ class Builder extends AbstractController
             'route' => 'app_logout',
             'extras' => [
                 'class' => 'opacity-25 pe-none',
-            ]
+            ],
+            'linkAttributes' => [
+                'data-bs-toggle' => 'tooltip',
+                'data-bs-title' => 'Producer Meeting',
+                'data-bs-placement' => 'right',
+            ],
         ]);
 
-        $menu->addChild('Events', [
+        $menu->addChild('SPUR1-RECORDS', [
             'route' => 'app_logout',
             'extras' => [
                 'class' => 'opacity-25 pe-none',
-            ]
-        ]);
-
-        $menu->addChild('Records', [
-            'route' => 'app_logout',
-            'extras' => [
-                'class' => 'opacity-25 pe-none',
-            ]
-        ]);
-
-        $menu->addChild('Player', [
-            'route' => 'app_logout',
-            'extras' => [
-                'class' => 'opacity-25 pe-none',
-            ]
+            ],
+            'linkAttributes' => [
+                'data-bs-toggle' => 'tooltip',
+                'data-bs-title' => 'SPUR1-RECORDS',
+                'data-bs-placement' => 'right',
+            ],
         ]);
 
         return $menu;
